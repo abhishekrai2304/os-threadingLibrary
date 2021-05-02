c14c2fecabfbd5c4642ee795ba51948befea9023
Shivani <sss432000@gmail.com>
2021-05-02 21:01:38 +0530

    Makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+run:
+	chmod +x automated_testing.sh
+	./automated_testing.sh
+
--- automated_testing.sh
+++ automated_testing.sh
@@ -1,5 +1,11 @@
-echo 
+echo ***********************Running Automated testing************************
+echo
+./autorun
+echo
+echo
+echo
+echo
+echo
 echo ***********************Running Automated Testing*************************
 echo
-
 ./q
